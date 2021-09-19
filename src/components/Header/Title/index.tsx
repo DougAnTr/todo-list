60eb2d1a69f3f05aec60723be2221ed01cb4c158
Douglas Trofino <douglas.trofino@outlook.com>
2021-09-19 12:02:37 -0300

    Header title

---
+++ src/components/Header/Title/index.tsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+import { Text } from './styles'
+
+export const Title: React.FC = ({children}) => {
+  return <Text>{children}</Text>
+}
---
+++ src/components/Header/Title/styles.tsx
@@ -0,0 +1,6 @@
+import styled from 'styled-components'
+
+export const Text = styled.h1`
+  font-weight: 600;
+  font-size: 26px;
+`
